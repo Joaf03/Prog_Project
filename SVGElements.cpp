dd6357052d2f1cb862d5563ccf7deb9ccf2f5900
Gustavo Lourenço <149388009+GustLou@users.noreply.github.com>
2024-05-15 20:54:54 +0100

    SVGElements.cpp

--- SVGElements.cpp
+++ SVGElements.cpp
@@ -3,9 +3,10 @@
 
 namespace svg
 {
+
     // These must be defined!
-    SVGElement::SVGElement() {}
-    SVGElement::~SVGElement() {}
+    SVGElement::SVGElement()  {} //!the constructor
+    SVGElement::~SVGElement() {} //! the destructor
 
     // Ellipse (initial code provided)
     Ellipse::Ellipse(const Color &fill,
@@ -44,6 +45,7 @@ namespace svg
     polyline::polyline(const Color &fill,
                        const std::vector<Point> &points)
                        :fill(fill), points(points){}
+
     void polyline::draw(PNGImage &img) const{
         for (size_t i = 0; i < points.size() - 1; i++ ){
             img.draw_line(points[i],points[i+1],fill); //! the function draw to each point
@@ -58,7 +60,7 @@ namespace svg
     line::line(const Point &start,
                const Point &end,
                const Color &fill)
-               :polyline(fill,std::vector<Point> {start,end}){}
+               :polyline(fill,std::vector<Point> {start,end}), start(start), end(end) {}
 
     void line::draw(PNGImage &img) const{
         img.draw_line(start,end,fill);
@@ -83,15 +85,13 @@ namespace svg
                const int &height)
                :polygon(fill, std::vector<Point>{
                 upper_left_corner,
-                Point(upper_left_corner.x + width, upper_left_corner.y), 
-                Point(upper_left_corner.x + width, upper_left_corner.y + height), 
-                Point(upper_left_corner.x, upper_left_corner.y + height)
+                {upper_left_corner.x + width, upper_left_corner.y}, 
+                {upper_left_corner.x + width, upper_left_corner.y + height}, 
+                {upper_left_corner.x, upper_left_corner.y + height}
                }) {}
     void rect::draw(PNGImage &img) const{
         img.draw_polygon(points,fill);
     }
-
-
 
     Group::Group(const std::vector<SVGElement*> &elements, const std::string &id)
         : SVGElement(Color{0,0,0}, id), elements(elements)
@@ -106,27 +106,19 @@ namespace svg
 
     void Group::translate(const Point &dir) {
         for(const SVGElement *element: elements){
-            element->translate(dir);
+            element->translate(dir.x, dir.y);
         }
     }
 
     void Group::rotate(const Point &origin, int degrees) {
         for(const SVGElement *element: elements){
-            element->rotate(origin, degrees);
+            element->rotate(degrees,origin);
         }
     }
     void Group::scale(const Point &origin, int factor) {
         for(const SVGElement *element: elements){
-            element->scale(origin , factor);
+            element->scale(origin, factor);
         }
     }
-
-
-
-
-
-
-
-
-
+    
 }
